88e455aec11cab2ad4783599819f7f1dda045ac8
chalab_k <khalis.chalabi@epita.fr>
2017-07-10 22:36:44 +0200

    Connection: save in the session

--- src/main/java/dao/UserAccess.java
+++ src/main/java/dao/UserAccess.java
@@ -3,9 +3,11 @@ package dao;
 import models.User;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.servlet.http.HttpSession;
 import java.io.Serializable;
 
 /**
@@ -19,8 +21,12 @@ public class UserAccess extends GenericAccess {
     public Boolean checkConnection(String email, String password) {
         Query query = em.createQuery("Select user from User user where user.email=:email");
         Object user = query.setParameter("email", email).getSingleResult();
-        if (((User) user).getPassword().equals(password))
+        if (((User) user).getPassword().equals(password)) {
+            Object session = FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+            ((HttpSession) session).setAttribute("userId", ((User) user).getId());
+            ((HttpSession) session).setAttribute("userRole", ((User) user).getRole());
             return true;
+        }
         return false;
     }
 }
