f228a07b3e9a3824e79f19738560a42f36ed9336
chalab_k <khalis.chalabi@epita.fr>
2017-07-10 23:06:13 +0200

    Blog: form for adding blog

--- src/main/java/controllers/BlogController.java
+++ src/main/java/controllers/BlogController.java
@@ -1,14 +1,19 @@
 package controllers;
 
+import lombok.Getter;
+import lombok.Setter;
 import models.Blog;
 import services.BlogService;
 import services.BlogService;
+import services.UserService;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -19,8 +24,16 @@ import java.util.List;
 @Named("blogController")
 public class BlogController
 {
+    @Getter @Setter
+    private String title;
+    @Getter @Setter
+    private String theme;
+
     @Inject
     private BlogService blogService;
+
+    @Inject
+    private UserService userService;
 
     private Blog blog;
 
@@ -34,6 +47,19 @@ public class BlogController
         {
             e.printStackTrace();
         }
+    }
+
+    public void add(Integer id) {
+        if (theme.isEmpty() || title.isEmpty()) {
+            addMessage("Fill all fields");
+            return;
+        }
+        blogService.add(userService.getById(id), theme, title, new Date());
+    }
+
+    public void addMessage(String summary) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_FATAL, summary,  null);
+        FacesContext.getCurrentInstance().addMessage(null, message);
     }
 
     public Blog getBlog()
